f675910f6c6a08ac48b172aa3222634eea3c0d96
Maxim Zebzeev <maksimzeb@yandex.ru>
2021-01-09 12:44:22 +0500

    added

--- javascript/sript.js
+++ javascript/sript.js
@@ -5,3 +5,19 @@ function openNav () {
 function closeNav() {
 	document.getElementById('myNav').style.height = "0%";
 }
+// кнопка верх 
+window.onscroll = function() {
+	doScroll ();
+}
+function doScroll() {
+	let windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
+	let count = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+	let countpro = (windowScroll/count) * 100;
+	let link = document.getElementById('scroll');
+	
+	if (countpro > 10) {
+		link.style.display = "block";
+	} else if (countpro < 10) {
+		link.style.display = "none";
+	}
+}
