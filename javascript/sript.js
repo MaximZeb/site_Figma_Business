7c6733319e3e2ec3c6ef8ac742b1ab66bd0b0f34
Maxim Zebzeev <maksimzeb@yandex.ru>
2021-01-11 22:22:07 +0500

    added

--- javascript/sript.js
+++ javascript/sript.js
@@ -21,3 +21,16 @@ function doScroll() {
 		link.style.display = "none";
 	}
 }
+//  плавное перемещение по якорю
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+	anchor.addEventListener('click', function(event){
+		event.preventDefault();
+		const blockID = anchor.getAttribute('href');
+		document.querySelector('' + blockID).scrollIntoView({
+			behavior: "smooth",
+			block: 'start'
+		});
+	});
+}
